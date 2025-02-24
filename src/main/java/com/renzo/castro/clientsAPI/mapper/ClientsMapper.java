04b41fa8fee482a724e5271e48fac7bb09816c7b
Renzo Castro <renzogustavocastro99@gmail.com>
2025-02-24 16:41:48 -0300

    Refactor: Update Clients Mapper

--- src/main/java/com/renzo/castro/clientsAPI/mapper/ClientsMapper.java
+++ src/main/java/com/renzo/castro/clientsAPI/mapper/ClientsMapper.java
@@ -1,4 +1,18 @@
 package com.renzo.castro.clientsAPI.mapper;
 
+import com.renzo.castro.clientsAPI.models.Clients;
+import com.renzo.castro.clientsAPI.models.dtos.UserResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
 public interface ClientsMapper {
+    ClientsMapper INSTANCE = Mappers.getMapper(ClientsMapper.class);
+
+    UserResponseDTO fromUserResponseDTO(Clients client);
+
+    Clients fromClients(UserResponseDTO userResponseDTO);
+
+    List<UserResponseDTO> fromListClients(List<Clients> Clients);
 }
