cb313d65871cf6f233d67815b16db00389dc59f7
Renzo Castro <renzogustavocastro99@gmail.com>
2025-02-25 23:20:46 -0300

    Refactor: Update LocationDTO

--- src/main/java/com/renzo/castro/clientsAPI/models/dtos/componentsApiResponse/LocationDTO.java
+++ src/main/java/com/renzo/castro/clientsAPI/models/dtos/componentsApiResponse/LocationDTO.java
@@ -1,9 +1,16 @@
 package com.renzo.castro.clientsAPI.models.dtos.componentsApiResponse;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LocationDTO {
+    private StreetDTO street;
     private String city;
+    private String state;
     private String country;
+    private String postcode;
+    private CoordinatesDTO coordinates;
+    private TimezoneDTO timezone;
 }
