6c9a1487e9cd1356f753fba8e1cb860efdefd000
Renzo Castro <renzogustavocastro99@gmail.com>
2025-02-25 23:25:13 -0300

    Refactor: Update WeatherController

--- src/main/java/com/renzo/castro/clientsAPI/controllers/WeatherController.java
+++ src/main/java/com/renzo/castro/clientsAPI/controllers/WeatherController.java
@@ -1,7 +1,10 @@
 package com.renzo.castro.clientsAPI.controllers;
+
 import com.renzo.castro.clientsAPI.models.Weather;
+import com.renzo.castro.clientsAPI.models.dtos.WeatherResponseDTO;
 import com.renzo.castro.clientsAPI.services.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -14,10 +17,29 @@ public class WeatherController {
     private WeatherService weatherService;
 
     /**
-     * Obtener el clima para un usuario específico
+     * Obtener el clima de un usuario por client_id
      */
     @GetMapping("/{clientId}")
-    public Optional<Weather> getWeatherByClientId(@PathVariable Long clientId) {
-        return weatherService.getWeatherByClientId(clientId);
+    public ResponseEntity<Weather> getWeatherByClientId(@PathVariable Long clientId) {
+        Optional<Weather> weather = weatherService.getWeatherByClientId(clientId);
+        return weather.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    /**
+     * Obtener el clima actual basado en una ubicación específica.
+     *
+     * @param latitude  Latitud de la ubicación.
+     * @param longitude Longitud de la ubicación.
+     * @param clientId id del cliente
+     * @return Clima actual.
+     */
+    @GetMapping
+    public ResponseEntity<Weather> getWeather(
+            @RequestParam("latitude") double latitude,
+            @RequestParam("longitude") double longitude,
+            @RequestParam("clientId") Long clientId) {
+
+        Weather weatherData = weatherService.fetchAndSaveWeather(latitude, longitude, clientId);
+        return ResponseEntity.ok(weatherData);
     }
 }
