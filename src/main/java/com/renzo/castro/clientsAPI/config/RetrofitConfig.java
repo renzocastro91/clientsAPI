6162e50acdfd5adf2f072f56185db29eeb33c999
Renzo Castro <renzogustavocastro99@gmail.com>
2025-02-25 23:21:38 -0300

    Refactor: Update RetrofitConfig

--- src/main/java/com/renzo/castro/clientsAPI/config/RetrofitConfig.java
+++ src/main/java/com/renzo/castro/clientsAPI/config/RetrofitConfig.java
@@ -1,19 +1,37 @@
 package com.renzo.castro.clientsAPI.config;
-import com.renzo.castro.clientsAPI.client.WeatherApiClient;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import com.renzo.castro.clientsAPI.clients.WeatherApiClient;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 @Configuration
 public class RetrofitConfig {
 
+    private static final String BASE_URL = "https://api.open-meteo.com/v1/";
+
     @Bean
-    public WeatherApiClient weatherApiClient() {
+    public Retrofit retrofit() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+
         return new Retrofit.Builder()
-                .baseUrl("https://api.open-meteo.com/")
+                .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(JacksonConverterFactory.create())
-                .build()
-                .create(WeatherApiClient.class);
+                .build();
+    }
+
+    @Bean
+    public WeatherApiClient weatherApiClient(Retrofit retrofit) {
+        return retrofit.create(WeatherApiClient.class);
     }
 }
+
