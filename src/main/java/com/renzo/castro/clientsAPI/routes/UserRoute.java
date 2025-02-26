2c7b5325e93371d04d7362d19afd56aa8a23c90d
Renzo Castro <renzogustavocastro99@gmail.com>
2025-02-25 23:22:40 -0300

    Refactor: Update UserRoute

--- src/main/java/com/renzo/castro/clientsAPI/routes/UserRoute.java
+++ src/main/java/com/renzo/castro/clientsAPI/routes/UserRoute.java
@@ -5,10 +5,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserRoute extends RouteBuilder {
 
+    private static final String API_URL = "https://randomuser.me/api/?results=";
+
     @Override
-    public void configure() throws Exception {
-        from("direct:fetchUser")
-                .toD("https://randomuser.me/api/?results=1")
-                .convertBodyTo(String.class);
+    public void configure() {
+        from("direct:getUsers")
+                .routeId("getUsersRoute")
+                .log("Llamando a la API de usuarios con ${header.results} resultados")
+                .toD(API_URL + "${header.results}")
+                .log("Respuesta de la API de usuarios: ${body}");
+
+        from("direct:fetchUser") // 🔹 Define la ruta esperada
+                .routeId("fetchUserRoute")
+                .log("Llamando a la API de usuarios")
+                .toD(API_URL) // 🔹 Llama a la API de randomuser.me
+                .log("Respuesta de la API de usuarios: ${body}");
     }
+
+
 }
