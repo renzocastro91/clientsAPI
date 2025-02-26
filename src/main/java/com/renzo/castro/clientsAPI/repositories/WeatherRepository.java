8ec6f926cd9142ef8341ccd8f474a06ab11b251d
Renzo Castro <renzogustavocastro99@gmail.com>
2025-02-25 23:19:58 -0300

    Feat: Add WeatherRepository

--- src/main/java/com/renzo/castro/clientsAPI/repositories/WeatherRepository.java
+++ src/main/java/com/renzo/castro/clientsAPI/repositories/WeatherRepository.java
@@ -2,6 +2,12 @@ package com.renzo.castro.clientsAPI.repositories;
 
 import com.renzo.castro.clientsAPI.models.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
+    Optional<Weather> findByClientId(@Param("clientId") Long clientId);
 }
