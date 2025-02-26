bf26c8ffaa0cdcfd888a570119a7eaad4dedc112
Renzo Castro <renzogustavocastro99@gmail.com>
2025-02-25 23:25:20 -0300

    Refactor: Update WeatherResponseDTO

--- src/main/java/com/renzo/castro/clientsAPI/models/dtos/WeatherResponseDTO.java
+++ src/main/java/com/renzo/castro/clientsAPI/models/dtos/WeatherResponseDTO.java
@@ -1,18 +1,20 @@
 package com.renzo.castro.clientsAPI.models.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.renzo.castro.clientsAPI.models.dtos.componentsApiResponse.CurrentWeatherDTO;
+import com.renzo.castro.clientsAPI.models.dtos.componentsApiResponse.CurrentWeatherUnitsDTO;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherResponseDTO {
+    private double latitude;
+    private double longitude;
+    private double generationtime_ms;
+    private int utc_offset_secondsM;
+    private String timezone;
+    private String timezone_abbreviation;
+    private double elevation;
+    private CurrentWeatherUnitsDTO current_weather_units;
     private CurrentWeatherDTO current_weather;
-
-    @Data
-    public static class CurrentWeatherDTO {
-        private String time;
-        private double temperature;
-        private double windspeed;
-        private int winddirection;
-        private boolean is_day;
-        private int weathercode;
-    }
 }
