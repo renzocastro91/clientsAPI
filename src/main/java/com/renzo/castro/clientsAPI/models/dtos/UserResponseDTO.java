02f6b08228ff030eb76a71f20fc049241fbaffbb
Renzo Castro <renzogustavocastro99@gmail.com>
2025-02-25 23:22:18 -0300

    Refactor: Update UserResponseDTO

--- src/main/java/com/renzo/castro/clientsAPI/models/dtos/UserResponseDTO.java
+++ src/main/java/com/renzo/castro/clientsAPI/models/dtos/UserResponseDTO.java
@@ -1,15 +1,18 @@
 package com.renzo.castro.clientsAPI.models.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.renzo.castro.clientsAPI.models.dtos.componentsApiResponse.*;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserResponseDTO {
     private String gender;
     private NameDTO name;
     private LocationDTO location;
-    private CoordinatesDTO coordinates;
     private String email;
     private LoginDTO login;
     private String phone;
+    private String cell;
 }
+
